aba0a684be08719f2a743a3382652699ff6eaf47
narendarputta <allunarendar@gmail.com>
2021-02-24 22:03:15 +0530

    final

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -159,6 +159,23 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ]
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+
 
 MEDIA_URL = '/media/'
 
